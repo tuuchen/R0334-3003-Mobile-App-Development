bee1995632a4362558db2442f3cbd6f82c7e717d
tuuchen <tuuchen@outlook.com>
2020-03-08 01:11:24 +0200

    Update

--- src/app/pages/menu/menu-routing.module.ts
+++ src/app/pages/menu/menu-routing.module.ts
@@ -5,8 +5,44 @@ import { MenuPage } from './menu.page';
 
 const routes: Routes = [
   {
+    path: 'menu',
+    component: MenuPage,
+    children: [
+      // Menu items
+      {
+        path: 'home',
+        loadChildren: '../home/home.module#HomePageModule'
+      },
+      {
+        path: 'login',
+        loadChildren: '../login/login.module#LoginPageModule'
+      },
+      {
+        path: 'register',
+        loadChildren: '../register/register.module#RegisterPageModule'
+      },
+      {
+        path: 'contact',
+        loadChildren: '../contact/contact.module#ContactPageModule'
+      },
+      // Tabs
+      {
+        path: 'tab1',
+        loadChildren: '../tab1/tab1.module#Tab1PageModule'
+      },
+      {
+        path: 'tab2',
+        loadChildren: '../tab2/tab2.module#Tab2PageModule'
+      },
+      {
+        path: 'tab3',
+        loadChildren: '../tab3/tab3.module#Tab3PageModule'
+      }
+    ]
+  },
+  {
     path: '',
-    component: MenuPage
+    redirectTo: '/menu/home'
   }
 ];
 
--- src/app/pages/menu/menu.module.ts
+++ src/app/pages/menu/menu.module.ts
@@ -1,63 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { MenuPageRoutingModule } from './menu-routing.module';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { MenuPage } from './menu.page';
 import { MenuController } from '@ionic/angular';
 
-// Changes made in MenuPageModule
-
-const routes: Routes = [
-  {
-    path: 'menu',
-    component: MenuPage,
-    children: [
-      // Menu items
-      {
-        path: 'home',
-        loadChildren: '../home/home.module#HomePageModule'
-      },
-      {
-        path: 'login',
-        loadChildren: '../login/login.module#LoginPageModule'
-      },
-      {
-        path: 'register',
-        loadChildren: '../register/register.module#RegisterPageModule'
-      },
-      {
-        path: 'contact',
-        loadChildren: '../contact/contact.module#ContactPageModule'
-      },
-      // Tabs
-      {
-        path: 'tab1',
-        loadChildren: '../tab1/tab1.module#Tab1PageModule'
-      },
-      {
-        path: 'tab2',
-        loadChildren: '../tab2/tab2.module#Tab2PageModule'
-      },
-      {
-        path: 'tab3',
-        loadChildren: '../tab3/tab3.module#Tab3PageModule'
-      }
-    ]
-  },
-  {
-    path: '',
-    redirectTo: '/menu/home'
-  }
-];
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
+  imports: [CommonModule, FormsModule, IonicModule, MenuPageRoutingModule],
   declarations: [MenuPage]
 })
 export class MenuPageModule {
